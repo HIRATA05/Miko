6b60c3c16d7156ddc7f3d0cc0956061778b64c34
Millan50 <1015aika256431@gmail.com>
2024-07-12 16:22:33 +0900

    maingame変更

---
+++ Assets/UnityChan2D/Demo/Scripts/EnemyController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+
+{
+    public GameObject Goal;
+
+    public float EnemySpeed = 1.0f;
+
+    private GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Move()
+    {
+        //transform.position
+    }
+
+}
